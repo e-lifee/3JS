19ad77f6f8b1a521ad73dfafb509e3ca7e2d9df5
elife efe <elifeefe@gmail.com>
2023-06-25 18:38:35 +0300

    Rotation

--- App.js
+++ App.js
@@ -1,5 +1,31 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas,useFrame } from '@react-three/fiber';
+import { useState,useRef } from 'react';
 
+function Box(props){
+  const [active,setActive]=useState(false);
+  const mesh=useRef();
+  
+  useFrame((state,delta)=>{
+    if(active){
+    mesh.current.rotation.x+=delta*5
+    {/*-/+ changes the rotate direction*/}
+    {/*delta*number changes the rotation speed */}
+    mesh.current.rotation.y-=delta
+    mesh.current.rotation.z+=delta
+  }
+  });
+
+  return (
+    <mesh 
+    {...props}
+    ref={mesh}
+    onClick={(event)=>setActive(!active)}
+    >
+      <boxGeometry/>
+      <meshStandardMaterial color={active?'blue':'grey'}/>
+    </mesh>
+  );
+}
 export default function App() {
   return( 
   <Canvas>
@@ -10,12 +36,12 @@ export default function App() {
   <meshStandardMaterial color={'red'} />
   </mesh> 
 
-  <mesh position={[1,2,0]}>
-    <boxGeometry/>
-    <meshStandardMaterial color={'blue'} />
-  </mesh> 
+  <Box position={[1,2,0]}/>
+  <Box position={[-1,2,0]}/>
+
+
   <mesh position={[-0.5,-2.5,0]} scale={0.08}>
-    <torusKnotGeometry radius={10} args={[10,1,260,6]}/>
+    <torusKnotGeometry radius={10} args={[10,1,260,6,10,16]}/>
     <meshStandardMaterial color={'green'} />
   </mesh> 
   </Canvas>
